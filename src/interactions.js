7ff6ede619dab4d00fbfad135002fc04c8483f36
Abdul Samad <samadmirza269@gmail.com>
2024-09-26 15:14:02 +0500

    Working in secondary sidear

---
+++ src/interactions.js
@@ -0,0 +1,33 @@
+//function to handle notifications
+function notificationHandler() {
+  const notificationWrapper = document.querySelector("[data-notification-wrapper]");
+  const notificationLink = document.querySelector("[data-notification-link]");
+  const sidebarNav = document.querySelector("[data-sidebar-nav]");
+
+  document.addEventListener("click", (event) => {
+    if (event.target.classList.contains("notifications-link") || event.target.closest(".notifications-link")) {
+      sidebarNav.classList.remove("overflow-hidden");
+      notificationWrapper.classList.add("show");
+      return;
+    }
+
+    sidebarNav.classList.add("overlow-hidden");
+    notificationWrapper.classList.remove("show");
+  });
+}
+
+//function to handle messages tabs
+function tabsHandler() {
+  const tabs = document.querySelectorAll(".tab");
+
+  tabs.forEach((tab, index, tabs) => {
+    tab.addEventListener("click", () => {
+      Array.from(tabs).filter(inactivetab => inactivetab !== tab).
+        forEach(incativetab => incativetab.classList.remove("show"));
+
+      tab.classList.add("show");
+    });
+  })
+}
+
+tabsHandler();
